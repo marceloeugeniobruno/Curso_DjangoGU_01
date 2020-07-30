40b1ee41767ed00a25e575a4bc103360dd67143f
marceloazevedo <marceloeugeniobruno@gmail.com>
2020-07-30 00:27:56 -0300

    Aula 29

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Produto, Cliente
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'estoque', 'proco')
+
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'sobrenome', 'email')
+
+
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Cliente, ClienteAdmin)
 
 # Register your models here.
